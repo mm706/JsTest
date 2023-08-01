cb1a99d3e4c74e973d077def0d1b35977fb3da4a
M.M Allahyari <36984791+mm706@users.noreply.github.com>
2023-08-01 19:36:48 +0330

    Add files via upload

--- js.js
+++ js.js
@@ -1,24 +1,28 @@
 let lBtn = document.getElementById('left');
-let rBtn = document.getElementById('right')
+let rBtn = document.getElementById('right');
 let pic = document.getElementById('one');
-let container = document.querySelector('.container')
-let img = document.querySelectorAll('.img')
+let container = document.querySelector('.container');
+let img = document.querySelectorAll('.img');
+let wrapper = document.querySelector('.wrapper');
 
 rBtn.addEventListener('click' , moveRight);
 lBtn.addEventListener('click' , moveLeft);
 
-let i =0;
 
-// if (i<3 || i>-1){
-   function moveRight(){
-      container.style.transform += 'translateX(-310px)';
-      i ++;
-   }
-   function moveLeft(){
-      container.style.transform += 'translateX(310px)';
-      i --;
-   }
-// }
+
+function moveRight(){
+   wrapper.scrollLeft += container.clientWidth;
+      lBtn.style.display = 'block';
+      rBtn.style.display = 'none';
+}
+function moveLeft(){
+   wrapper.scrollLeft -= container.clientWidth;
+      lBtn.style.display = 'none';
+      rBtn.style.display = 'block';
+
+
+}
+
 
 
 
